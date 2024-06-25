5e9f567334251ab4e05a08c08ffcee971d56316e
Agusti Bayu <80197846+AgustiBayu@users.noreply.github.com>
2024-06-26 01:17:35 +0700

    update variabel

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        var age = 23;
         Variabel t = new Variabel();
         t.setNamaDepan("Agusti Bayu");
         t.setNamaBelakang("Samudro");
         System.out.println("perkenalkan nama saya " + t.getNamaDepan() + " " + t.getNamaBelakang());
+
+        System.out.println("umur saya " + age);
     }
 }
