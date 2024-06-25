65811cf814dfce725a72fa982d3b53497d4b650e
Agusti Bayu <80197846+AgustiBayu@users.noreply.github.com>
2024-06-26 01:11:11 +0700

    variabel string

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Variabel t = new Variabel();
+        t.setNamaDepan("Agusti Bayu");
+        t.setNamaBelakang("Samudro");
+        System.out.println("perkenalkan nama saya " + t.getNamaDepan() + " " + t.getNamaBelakang());
+    }
+}
---
+++ src/main/java/org/example/Variabel.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Variabel {
+    private String namaDepan;
+    private String namaBelakang;
+
+    public String getNamaDepan() {
+        return namaDepan;
+    }
+
+    public void setNamaDepan(String namaDepan) {
+        this.namaDepan = namaDepan;
+    }
+
+    public String getNamaBelakang() {
+        return namaBelakang;
+    }
+
+    public void setNamaBelakang(String namaBelakang) {
+        this.namaBelakang = namaBelakang;
+    }
+}
